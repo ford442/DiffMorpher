45553294a87cdf38b8fd8669344f08412771cb0d
Noah Cohn <noahc42@gmail.com>
2025-11-01 21:33:26 -0700

    Update lora_utils.py

--- utils/lora_utils.py
+++ utils/lora_utils.py
@@ -176,14 +176,14 @@ def train_lora(
             )
         
         # This is for SDXL SELF-ATTENTION
-        else:
-            if hasattr(F, "scaled_dot_product_attention"):
-                lora_attn_processor_class = LoRAAttnProcessor
-                unet_lora_attn_procs[name] = lora_attn_processor_class(
-                    hidden_size=hidden_size, 
-                    cross_attention_dim=cross_attention_dim, 
-                    rank=lora_rank
-                )
+       else:
+            # ALWAYS use LoRAAttnProcessor. It IS a module and works.
+            lora_attn_processor_class = LoRAAttnProcessor
+            unet_lora_attn_procs[name] = lora_attn_processor_class(
+                # LoRAAttnProcessor does NOT take hidden_size
+                cross_attention_dim=cross_attention_dim, 
+                rank=lora_rank
+            )
             else:
                 lora_attn_processor_class = LoRAAttnProcessor
                 unet_lora_attn_procs[name] = lora_attn_processor_class(
