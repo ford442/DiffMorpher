03d1200f5ac4c22195ded93f5cf7396cc4c0ad97
Noah Cohn <noahc42@gmail.com>
2025-11-02 19:41:39 -0800

    Update model.py

--- model.py
+++ model.py
@@ -393,9 +393,9 @@ class DiffMorpherPipelineXL(StableDiffusionXLPipeline):
                 weight_name = f"{output_path.split('/')[-1]}_lora_0_xl.ckpt" # SDXL Change
                 load_lora_path_0 = save_lora_dir + "/" + weight_name
                 if not os.path.exists(load_lora_path_0):
-                    train_lora(    image=img_0,     prompt=prompt_0,     save_lora_dir=save_lora_dir,     text_encoder=self.text_encoder, 
-                        text_encoder_2=self.text_encoder_2,     tokenizer=self.tokenizer,     tokenizer_2=self.tokenizer_2,     vae=self.vae, 
-                        unet=self.unet,     noise_scheduler=self.scheduler,     lora_steps=lora_steps,     lora_lr=lora_lr,     lora_rank=lora_rank,     weight_name=weight_name)
+                    train_lora( image=img_0, prompt=prompt_0, save_lora_dir=save_lora_dir, text_encoder=self.text_encoder, 
+                        text_encoder_2=self.text_encoder_2, tokenizer=self.tokenizer, tokenizer_2=self.tokenizer_2, vae=self.vae, 
+                        unet=self.unet, lora_steps=lora_steps, lora_lr=lora_lr, lora_rank=lora_rank, weight_name=weight_name)
             
             lora_0 = torch.load(load_lora_path_0, map_location="cpu")
 
