68e772ea8c7013aa24f5439424d8d0d0c61a8a16
Noah Cohn <noahc42@gmail.com>
2025-11-07 00:58:35 -0800

    Update model.py

--- model.py
+++ model.py
@@ -291,7 +291,7 @@ class DiffMorpherPipelineXL(StableDiffusionXLPipeline):
                 noise_pred_uncon, noise_pred_con = noise_pred.chunk(2, dim=0)
                 noise_pred = noise_pred_uncon + guidance_scale * (noise_pred_con - noise_pred_uncon)
             
-            latents = self.scheduler.step(noise_pred, t, latents, return_dict=False) 0 
+            latents = self.scheduler.step(noise_pred, t, latents, return_dict=False)
         return latents
 
     # SDXL Change: New function to get dual embeddings
